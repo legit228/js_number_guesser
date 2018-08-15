8334ea4ffa6c790dafde4b0d78d2c7decaa63f36
Chris <41971210+legit228@users.noreply.github.com>
2018-08-14 20:21:23 -0500

    Added initial files

---
+++ guessing.js
@@ -0,0 +1,22 @@
+var goal = Math.floor((Math.random() * 100) + 1);
+var count=0;
+
+function checkGuess(event){
+  if(event.key==="Enter"){
+    checkGuess();
+  }
+}
+function checkGuess() {
+  var guess = parseInt(document.getElementById('txtGuess').value)
+  count++;
+  if(goal>guess)
+    alert("Higher!");
+  else if (goal<guess)
+    alert("Lower!");
+  if(goal===guess){
+    if(count===1)
+      document.write("You guessed the the number \""+goal+"\" in "+count+" guess!");
+    else
+      document.write("You guessed the the number \""+goal+"\" in "+count+" guesses!");
+  }
+}
