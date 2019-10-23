a24585783538c69382937f35b5089bed23e394c5
Chris <41971210+legit228@users.noreply.github.com>
2019-10-23 10:39:08 -0400

    Update guessing.js
    Comments were added to every step to explain what was happening. A redundant if statement was replaced with an else statement instead.

--- guessing.js
+++ guessing.js
@@ -1,16 +1,34 @@
+//Inputs user's maximum desired value and randomly chooses a number from 1 to their number
 var goal = Math.floor((Math.random() * parseInt(prompt("What should be the highest possible number?"))) + 1);
+
+//Initializes count of guesses equal to 0
 var count=0;
 
+//Function to check if the guess is too high, too low, or correct
 function checkGuess() {
+  
+  //Gets the guess out of a text box
   var guess = parseInt(document.getElementById('txtGuess').value)
+  
+  //Add 1 to the toal number of guesses
   count++;
+  
+  //Check if the guess is too low
   if(goal>guess)
     alert("Higher!");
+  
+  //Check if the guess is too low
   else if (goal<guess)
     alert("Lower!");
-  if(goal===guess){
+  
+  //The guess was correct
+  else {
+    
+    //The guess was correct in one attempt
     if(count===1)
       document.getElementById("lblCorrect").innerHTML = "You guessed the number \""+goal+"\" in "+count+" guess!";
+    
+    //The guess was correct in more than one attempt
     else
       document.getElementById("lblCorrect").innerHTML = "You guessed the number \""+goal+"\" in "+count+" guesses!";
   }
